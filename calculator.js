e93a8d7c8f645df0f90d4a1f37ed2b54740df255
Victor Dudochkin <dudochkin.victor@gmail.com>
2013-05-15 14:42:32 +0800

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,204 @@
+/*
+ * Based on the declarative calculator example in the Qt source tree
+ *
+ * original copyright:
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies)
+ *
+ * new code copyright:
+ * Copyright 2010 Intel Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+var curVal = 0
+var memory = 0
+var lastOp = ""
+var timer = 0
+var errorFlag = false
+
+function disabled(op) {
+    if (op == localeHelper.decimalPoint() && display.text.toString().search("\\" + localeHelper.decimalPoint()) != -1) {
+        // if we're starting a new number, allow it
+        if (lastOp.toString().length == 1 && ((lastOp >= "0" && lastOp <= "9") || lastOp == localeHelper.decimalPoint())) {
+            return true
+        }
+    } else if (op == squareRoot &&  display.text.toString().search(/-/) != -1) {
+        return true
+    } else if (errorFlag) {
+        if (op != "C" && op != "AC") // The display should stay at "Infinity" unless C or AC is pressed
+            return true
+    }
+    return false
+}
+
+function correctText() {
+    errorFlag = true
+    // correct the output to i18nable string when text
+    if ((display.text == "Infinity") || (display.text == "inf")) {
+        display.text = qsTr("Infinity")
+    } else if (display.text == "-Infinity") {
+        display.text = qsTr("-Infinity")
+    } else if ((display.text == "NaN") || (display.text == "nan")) {
+        display.text = qsTr("Error")
+    } else {
+        display.text = display.text.replace(".", localeHelper.decimalPoint())
+        errorFlag = false
+    }
+    if (errorFlag)
+        display.currentOperation.text = ""
+}
+
+// Perform any input action (number, decimal point, +/- or left arrow)
+function doInputOperation(op) {
+    var ok = true
+    if (op.toString().length==1 && ((op >= "0" && op <= "9") || op == localeHelper.decimalPoint()) ) {
+        if (lastOp.toString().length == 1 && ((lastOp >= "0" && lastOp <= "9") || lastOp == localeHelper.decimalPoint()) ) {
+            // don't append to number if it's too long
+            if (display.text.toString().length >= 14)
+                return ok;
+
+            display.text = (display.text == "0" && op != localeHelper.decimalPoint() ? op : display.text + op.toString())
+        } else {
+            display.text = (op == localeHelper.decimalPoint() ? "0":"") + op
+        }
+        lastOp = op
+        correctText()
+    } else if (op == plusminus) {
+        if (display.text.toString().charAt(0) == '-')
+            display.text = display.text.toString().substring(1)
+        else if (display.text != "0")
+            display.text = "-" + display.text
+    } else if (op == leftArrow) {
+        var length = display.text.toString().length
+        if (length == 1 || length == 2 && display.text.toString().charAt(0) == '-' || length == 3 && display.text == ("-0" + localeHelper.decimalPoint())) {
+            display.text = "0"
+        } else {
+            display.text = display.text.toString().slice(0, -1)
+        }
+    } else {
+        ok = false
+    }
+    return ok
+}
+
+// Perform any operation with single operand like abs, sqrt, 1/x and so on
+function doSingleOperation(op) {
+    var ok = true
+    if (op == "1/x") {
+        display.text = (1 / display.text.valueOf()).toString()
+    } else if (op == "x^2") {
+        display.text = (display.text.valueOf() * display.text.valueOf()).toString()
+    } else if (op == "Abs") {
+        display.text = (Math.abs(display.text.valueOf())).toString()
+    } else if (op == "Int") {
+        display.text = (Math.floor(display.text.valueOf())).toString()
+    } else if (op == squareRoot) {
+        display.text = (Math.sqrt(display.text.valueOf())).toString()
+    } else {
+        ok = false
+    }
+    if (ok) {
+        lastOp = op
+        correctText()
+    }
+    return ok
+}
+
+// Perform any operation with two operands like +, -, * or /
+function doDoubleOperation(op) {
+    var arithmeticOperations = ["+", "-", multiplication, division, "="]
+
+    var operationIndex = arithmeticOperations.indexOf(op)
+    if (operationIndex < 0)
+        return false
+
+    var curOperationIndex = arithmeticOperations.indexOf(display.currentOperation.text)
+    switch (curOperationIndex) {
+    case 0:                                                                         // +
+        display.text = Number(display.text.valueOf()) + Number(curVal.valueOf())
+        break
+    case 1:                                                                         // -
+        display.text = Number(curVal) - Number(display.text.valueOf())
+        break
+    case 2:                                                                         // multiplication
+        display.text = Number(curVal) * Number(display.text.valueOf())
+        break
+    case 3:                                                                         // division
+        display.text = Number(Number(curVal) / Number(display.text.valueOf())).toString()
+        break
+    }
+
+    if (operationIndex != arithmeticOperations.indexOf("=")) { // op != "="
+        curVal = display.text.valueOf()
+        display.currentOperation.text = op
+    } else {                                                  // op == "="
+        curVal = 0
+        display.currentOperation.text = ""
+    }
+    lastOp = op
+    correctText()
+
+    return true
+}
+
+function doMemoryOperation(op) {
+    var ok = true
+    if (op == qsTr("mc")) {
+        memory = 0;
+    } else if (op == qsTr("m+")) {
+        memory += Number(display.text)
+    } else if (op == qsTr("mr")) {
+        display.text = memory.toString()
+    } else if (op == qsTr("m-")) {
+        memory = Number(display.text)
+    } else if (op == qsTr("C")) {
+        display.text = "0"
+    } else if (op == qsTr("AC")) {
+        curVal = 0
+        memory = 0
+        lastOp = ""
+        display.text = "0"
+        display.currentOperation.text = ""
+    } else {
+        ok = false
+    }
+    if (ok) {
+        lastOp = op
+        correctText()
+    }
+    return ok
+}
+
+function doOperation(op) {
+    if (disabled(op)) {
+        return
+    }
+
+    if (errorFlag)
+        errorFlag = false
+
+    if (!doInputOperation(op)) {
+        display.text = display.text.replace(localeHelper.decimalPoint(), ".")               // Prepare for calculation
+        if (!doSingleOperation(op) && !doMemoryOperation(op) && !doDoubleOperation(op)) {
+            if (op == rotateLeft) {
+                main.state = 'rotated'
+            } else if (op == rotateRight) {
+                main.state = ''
+            } else {
+                console.log("Calculator.js: doOperation - Undefined operation: " + op)
+            }
+        }
+    }
+}
---
+++ ts/meego-app-calculator.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>calculator</name>
+    <message>
+        <location filename="../../calculator.js" line="50"/>
+        <source>Infinity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../calculator.js" line="52"/>
+        <source>-Infinity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../calculator.js" line="54"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../calculator.js" line="162"/>
+        <source>mc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../calculator.js" line="164"/>
+        <source>m+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../calculator.js" line="166"/>
+        <source>mr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../calculator.js" line="168"/>
+        <source>m-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../calculator.js" line="170"/>
+        <source>C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../calculator.js" line="172"/>
+        <source>AC</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../main.qml" line="59"/>
+        <source>Calculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="91"/>
+        <source>C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="92"/>
+        <source>AC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="99"/>
+        <source>mc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="100"/>
+        <source>m+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="101"/>
+        <source>m-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="102"/>
+        <source>mr</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
